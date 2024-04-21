e437db636b0d412b11c668a731970626fa55aaa9
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2024-04-21 13:59:15 -0300

    s

--- index.php
+++ index.php
@@ -4,45 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
     <title>Evotron</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark pb-5">
-        <a class="navbar-brand ml-4 mr-5" href="#">E<span class="span-verde">V</span>OTRON</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div    >
-        <div class="collapse navbar-collapse ml-5 mr-5" id="navbarText">
-          <ul class="navbar-nav mr-auto ml-5 mr-5">
-            <li class="nav-item active ml-5 mr-5">
-              <a class="nav-link ml-5 mr-5" href="#">Primeiros passos<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item ml-5 mr-5">
-              <a class="nav-link  ml-5 mr-5" href="#">Contato</a>
-            </li>
-            <li class="nav-item ml-5 mr-5">
-              <a class="nav-link  ml-5 mr-5" href="#">Sobre</a>
-            </li>
-          </ul>
-          <nav class="navbar navbar-dark bg-none ml-5 mr-5">
-            <form class="form-inline">
-            <div class="container-fluid">
-              <a href="cadastro.php"><button class="btn btn-success btn-lg" type="button">Cadastre-se</button></a>
-            </div>
-        </div>
-      </nav>
-    <div id="continua-header" class="container-fluid pt-5 pb-5">
-        <h1 id="header-h1" class="container-fluid">E<span class="span-verde">V</span>OTR<span class="span-roxo">O</span>N</h1>
-          <h2 id="header-h2" class="container-fluid pb-5">
-            Decida, se dedique e evolua!
-            <br>
-            Interatividade e Tecnologia
-          </h2>
+  </head>
+  <body>
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">E<span class="span-verde">V</span>OTRON</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Primeiros passos<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sobre</a>
+        </li>
+      </ul>
+      <a href="cadastro.html"><button class="btn btn-success btn-lg">Cadastre-se</button></a>
     </div>
+  </nav>
+  
+  <div id="continua-header" class="container-fluid pt-5 pb-5">
+    <h1 id="header-h1" class="text-center">E<span class="span-verde">V</span>OTR<span class="span-roxo">O</span>N</h1>
+    <h2 id="header-h2" class="text-center pb-5">
+      Decida, se dedique e evolua!
+      <br>
+      Interatividade e Tecnologia
+    </h2>
+  </div>
+  </body>
     <main>
 
         <div id="transicao-main">
