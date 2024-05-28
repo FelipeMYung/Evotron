aea38a4975c5930ecc5244b5a912d5a050dae3db
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2024-05-28 20:14:39 -0300

    Base
    Separação dos blocos macro da tela inicial

--- Home.php
+++ Home.php
@@ -6,6 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <img src="" alt="logo" srcset="">
+        <div id="Menu_Header">
+            <h3><a href="#">Login</a></h3>
+            <h3><a href="#">Sobre</a></h3>
+            <h3><a href="#">Contato</a></h3>
+        </div>
+    </header>
+    <main>
+        <h1>Olá Usuário!</h1>
+        <div id="principal">
+            <div id="tela1">
+                <div id="organizacao">
+                    <h2>Organização: </h2>
+                </div>
+            </div>
+            <div id="tela2">
+                <div id="Agenda">
+                    <h2>Agenda: </h2>
+                </div>
+                <div id="Progresso">
+                    <h2>Progresso: </h2>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
