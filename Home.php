d1b6d581b62eb3f0870eace1e1a31c613dcc47e0
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2024-05-29 15:38:41 -0300

    Tela inicial v1
    tela inicial, primeiro modelo

--- Home.php
+++ Home.php
@@ -20,14 +20,71 @@
             <div id="tela1">
                 <div id="organizacao">
                     <h2>Organização: </h2>
+                    <div id="tarefas">
+                        <div id="tarefas_nav">
+                            <h4>Tarefas: </h4>
+                            <button><p>Todos</p></button>
+                            <button><p>Prioridade</p></button>
+                            <button><p>Sla</p></button>
+                        </div>
+                        <ul>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <li>tarefa</li>
+                            <p></p>
+                            <button><p>Mais</p></button>
+                        </ul>
+                    </div>
+                    <div id="anotacoes">
+                        <input type="text">
+                        <p>Anotações de hoje: X</p>
+                        <button><p>Mais</p></button>
+                    </div>
+                    <h4>Dias ativos: X</h4>
+                    <img src="#" alt="grafico">
                 </div>
             </div>
             <div id="tela2">
                 <div id="Agenda">
                     <h2>Agenda: </h2>
+                    <div id="compromissos">
+                        <h4>Hoje: </h4>
+                        <button><p>Mais</p></button>
+                        <p>Dentista - 15:00</p>
+                        <p>Reunião - 18:00</p>
+                        <h4>Próximos dias: </h4>
+                        <p>Alistamento 18/06 - 13:00</p>
+                        <p>Trabalho final 10/11 - 23:59</p>
+                    </div>
+                    <div id="calendario">
+                        <img src="#" alt="calendario">
+                    </div>
                 </div>
                 <div id="Progresso">
                     <h2>Progresso: </h2>
+                    <button><p>Mais</p></button>
+                    <div id="progresso_nav">
+                        <button><p>Físico</p></button>
+                        <button><p>Emocional</p></button>
+                        <button><p>Inteligência</p></button>
+                    </div>]
+                    <div id="progresso_atual">
+                        <h4>Hoje: </h4>
+                        <p>Peso: </p>
+                        <p>Altura: </p>
+                        <p>Tempo: </p>
+                        <p></p>
+                    </div>
                 </div>
             </div>
         </div>
