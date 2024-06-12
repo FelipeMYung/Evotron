b161d7b1636372fd4cafb201fe0e293e51942ba7
Roberto M. Leitão <93278319+betoleitao5@users.noreply.github.com>
2024-06-12 10:04:59 -0300

    alteração css

---
+++ Sim.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <header>
+      <nav>
+        <p><a href="#">Login</a></p>
+        <p><a href="#">Sobre</a></p>
+        <p><a href="#">Contato</a></p>
+      </nav>
+    </header>
+</body>
+</html>
