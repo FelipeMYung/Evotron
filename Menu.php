94a206fb83fc2ee39aff1e36190ec9f4eedde893
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2024-06-22 12:41:27 -0300

    teste1 add filter

--- Menu.php
+++ Menu.php
@@ -1,77 +1,72 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <div class="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-            </svg>
-            <span>Olá, Usuário</span> <!-- A mensagem "Olá, Usuário" deve ser dinâmica -->
-        </div>
-        <nav>
-            <ul>
-            <li ><a style="color:darkgrey;" href="Menu.php">HOME</a></li>
-                <li><a href="index.php">LOGIN</a></li>
-                <li><a href="Sobre.php">SOBRE</a></li>
-                <li><a href="#">CONTATO</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <div class="popup-background" id="popup-background">
-            <div class="popup-main">
-                <span id="popup-close">x</span>
-                <h1 style="color: black;">Adicionar Tarefa:</h1>
-                <form method="post">
-                    <label for="titulo">Título:</label>
-                    <input type="text" id="titulo" name="titulo" required>
-                    <label for="descricao">Descrição:</label>
-                    <input type="text" id="descricao" name="descricao">
-                    <label for="data_vencimento">Data de Vencimento:</label>
-                    <input type="date" id="data_vencimento" name="data_vencimento">
-                    <select name="tipo_dado" id="tipo_dado">
-                        <option value="all">Todos</option>
-                        <option value="routine">Rotina</option>
-                        <option value="new">Novos</option>
-                    </select>
-                    <input type="submit" name="adicionar_tarefa" value="Adicionar">
-                </form>
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                <span>Olá, Usuário</span> <!-- A mensagem "Olá, Usuário" deve ser dinâmica -->
             </div>
-        </div>
-        <div class="popup-background" id="agenda-popup-background">
-            <div class="popup-main">
-                <span id="agenda-popup-close">x</span>
-                <h1 style="color: black;"> Adicionar Evento na Agenda: </h1>
-                <form method="post" action="" id="AgendaInputs">
-                    <label for="evento">Novo Evento:</label>
-                    <input type="text" id="evento" name="novoEvento" required>
-                    <label for="dia">Dia:</label>
-                    <input type="date" id="dia" name="diaEvento" required>
-                    <label for="hora">Hora:</label>
-                    <input type="time" id="hora" name="horaEvento" required>
-                    <input type="submit" value="Adicionar" name="adicionar_evento">
-                </form>
+            <nav>
+                <ul>
+                <li ><a style="color:darkgrey;" href="Menu.php">HOME</a></li>
+                    <li><a href="index.php">LOGIN</a></li>
+                    <li><a href="Sobre.php">SOBRE</a></li>
+                    <li><a href="#">CONTATO</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div class="popup-background" id="popup-background">
+                <div class="popup-main">
+                    <span id="popup-close">x</span>
+                    <h1 style="color: black;">Adicionar Tarefa:</h1>
+                    <form method="post">
+                        <label for="titulo">Título:</label>
+                        <input type="text" id="titulo" name="titulo" required>
+                        <label for="descricao">Descrição:</label>
+                        <input type="text" id="descricao" name="descricao">
+                        <label for="data_vencimento">Data de Vencimento:</label>
+                        <input type="date" id="data_vencimento" name="data_vencimento">
+                        <select name="tipo_dado" id="tipo_dado">
+                            <option value="routine">Rotina</option>
+                            <option value="new">Novos</option>
+                        </select>
+                        <input type="submit" name="adicionar_tarefa" value="Adicionar">
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="organization-container container">
+            <div class="popup-background" id="agenda-popup-background">
+                <div class="popup-main">
+                    <span id="agenda-popup-close">x</span>
+                    <h1 style="color: black;"> Adicionar Evento na Agenda: </h1>
+                    <form method="post" action="" id="AgendaInputs">
+                        <label for="evento">Novo Evento:</label>
+                        <input type="text" id="evento" name="novoEvento" required>
+                        <label for="dia">Dia:</label>
+                        <input type="date" id="dia" name="diaEvento" required>
+                        <label for="hora">Hora:</label>
+                        <input type="time" id="hora" name="horaEvento" required>
+                        <input type="submit" value="Adicionar" name="adicionar_evento">
+                    </form>
+                </div>
+            </div>
+            <div class="organization-container container">
             <h2>ORGANIZAÇÃO</h2>
             <div class="tasks-container">
                 <h3>TAREFAS:</h3>
-                <button class="filter">
-                    <span class="task-text">Todos</span>
-                </button>
-                <button class="filter">
-                    <span class="task-text">Rotina</span>
-                </button>
-                <button class="filter">
-                    <span class="task-text">Novos</span>
-                </button>
+                <form method="GET" action="">
+                    <button type="submit" name="filter" value="all">Todos</button>
+                    <button type="submit" name="filter" value="new">Novos</button>
+                    <button type="submit" name="filter" value="routine">Rotina</button>
+                </form>
             </div>
             <div class="tasks-container2">
                 <button id="add-task-button">Adicionar Tarefa</button>
@@ -80,58 +75,57 @@
                 <?php
                 include 'functions.php';
 
-                // Busca todas as tarefas do banco de dados
-                $tarefas = buscarTarefas();
+                $filtro = isset($_GET['filter']) ? $_GET['filter'] : 'all';
+                $tarefas = buscarTarefas($filtro);
 
-                // Exibe as tarefas
                 foreach ($tarefas as $tarefa) {
                     echo "<li>
-                     <div class='task_block'>{$tarefa['title']} <br> {$tarefa['description']} <br> Data de Vencimento: {$tarefa['due_date']} <input type='checkbox' value='completo' name='completo'></div>
+                    <div class='task_block'>{$tarefa['title']} <br> {$tarefa['description']} <br> Data de Vencimento: {$tarefa['due_date']} <input type='checkbox' value='completo' name='completo'></div>
                     </li>";
                 }
                 ?>
             </ul>
-            <form method="post" class="notes-container">
-                <input type="text" id="main_notes"  name="conteudoNota">
-                <button id="note_button" name="adicionar_nota"><p>Enviar</p></button>
-            </form>
-        </div>
-        <div class="sidebar-container">
-            <div class="container schedule-container">
-                <h2>AGENDA</h2>
-                <button id="add-event-button">Adicionar Evento</button>
-                <ul>
-                <?php
-                $eventos = buscarEvento();
-                foreach($eventos as $evento){
-                    echo "<li>{$evento['title']} - {$evento['date_day']} - {$evento['date_hour']}</li>";
-                }
-                ?>
-            </ul>
+                <form method="post" class="notes-container">
+                    <input type="text" id="main_notes"  name="conteudoNota">
+                    <button id="note_button" name="adicionar_nota"><p>Enviar</p></button>
+                </form>
             </div>
-            <div class="container progress-container">
-                <h2>PROGRESSO</h2>
-                <div class="progress-container">
-                    <div class="progress-bar">
-                        <div class="progress-bar-filled" style="width: 75%"></div>
-                    </div>
-                    <span>Físico</span>
+            <div class="sidebar-container">
+                <div class="container schedule-container">
+                    <h2>AGENDA</h2>
+                    <button id="add-event-button">Adicionar Evento</button>
+                    <ul>
+                    <?php
+                    $eventos = buscarEvento();
+                    foreach($eventos as $evento){
+                        echo "<li>{$evento['title']} - {$evento['date_day']} - {$evento['date_hour']}</li>";
+                    }
+                    ?>
+                </ul>
                 </div>
-                <div class="progress-container">
-                    <div class="progress-bar">
-                        <div class="progress-bar-filled" style="width: 50%"></div>
+                <div class="container progress-container">
+                    <h2>PROGRESSO</h2>
+                    <div class="progress-container">
+                        <div class="progress-bar">
+                            <div class="progress-bar-filled" style="width: 75%"></div>
+                        </div>
+                        <span>Físico</span>
                     </div>
-                    <span>Mental</span>
-                </div>
-                <div class="progress-container">
-                    <div class="progress-bar">
-                        <div class="progress-bar-filled" style="width: 90%"></div>
+                    <div class="progress-container">
+                        <div class="progress-bar">
+                            <div class="progress-bar-filled" style="width: 50%"></div>
+                        </div>
+                        <span>Mental</span>
                     </div>
-                    <span>Estudos</span>
+                    <div class="progress-container">
+                        <div class="progress-bar">
+                            <div class="progress-bar-filled" style="width: 90%"></div>
+                        </div>
+                        <span>Estudos</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-    <script src="script.js"></script>
-</body>
-</html>
+        </main>
+        <script src="script.js"></script>
+    </body>
+    </html>
--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-// Conexão com o banco de dados
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,21 +6,24 @@ $dbname = "organizacao_tarefas";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Função para buscar todas as tarefas no banco de dados
-function buscarTarefas() {
-    global $conn; // Tornar $conn global dentro da função
+function buscarTarefas($filtro) {
+    global $conn;
 
-    // Verifica se a conexão com o banco de dados foi estabelecida corretamente
     if (!$conn) {
         die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
     }
 
     $sql = "SELECT title, description, due_date, typeOfData FROM tasks";
+    if ($filtro == 'new') {
+        $sql .= " WHERE typeOfData = 'new'";
+    } elseif ($filtro == 'routine') {
+        $sql .= " WHERE typeOfData = 'routine'";
+    }
+
     $result = $conn->query($sql);
 
     if ($result === false) {
@@ -39,11 +41,9 @@ function buscarTarefas() {
     return $tarefas;
 }
 
-// Função para buscar todos os eventos no banco de dados
 function buscarEvento() {
-    global $conn; // Tornar $conn global dentro da função
+    global $conn;
 
-    // Verifica se a conexão com o banco de dados foi estabelecida corretamente
     if (!$conn) {
         die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
     }
@@ -65,10 +65,10 @@ function buscarEvento() {
 
     return $eventos;
 }
-function buscarNotas() {
-    global $conn; // Tornar $conn global dentro da função
 
-    // Verifica se a conexão com o banco de dados foi estabelecida corretamente
+function buscarNotas() {
+    global $conn;
+
     if (!$conn) {
         die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
     }
@@ -89,66 +89,5 @@ function buscarNotas() {
     }
 
     return $notas;
-}
-
-// Verifica se o formulário de adicionar tarefa foi submetido
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["adicionar_tarefa"])) {
-    // Verifica se todos os campos estão preenchidos
-    if (!empty($_POST["titulo"]) && !empty($_POST["descricao"]) && !empty($_POST["data_vencimento"])) {
-        // Obtém os dados do formulário
-        $titulo = $conn->real_escape_string($_POST["titulo"]);
-        $descricao = $conn->real_escape_string($_POST["descricao"]);
-        $data_vencimento = $conn->real_escape_string($_POST["data_vencimento"]);
-        $tipoDado = $conn->real_escape_string($_POST["tipo_dado"]);
-
-        // Verifica se a tarefa já existe no banco de dados
-        $sql_check = "SELECT * FROM tasks WHERE title='$titulo'";
-        $result_check = $conn->query($sql_check);
-        if ($result_check->num_rows == 0) {
-            // Adiciona a tarefa no banco de dados
-            $sql_insert = "INSERT INTO tasks (title, description, due_date, typeOfData) VALUES ('$titulo', '$descricao', '$data_vencimento', ' $tipoDado')";
-            if ($conn->query($sql_insert) === TRUE) {
-            } else {
-                echo "Erro ao adicionar tarefa: " . $conn->error;
-            }
-        } else {
-            
-        }
-    } else {
-        echo "Por favor, preencha todos os campos do formulário de adicionar tarefa.";
-    }
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["adicionar_evento"])) {
-    if (!empty($_POST["novoEvento"]) && !empty($_POST["diaEvento"]) && !empty($_POST["horaEvento"])) {
-        $titulo = $conn->real_escape_string($_POST["novoEvento"]);
-        $data = $conn->real_escape_string($_POST["diaEvento"]);
-        $hora = $conn->real_escape_string($_POST["horaEvento"]);
-
-
-        $sql = "INSERT INTO Events_T (title, date_day, date_hour) VALUES ('$titulo', '$data', '$hora')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Novo evento adicionado com sucesso!";
-        } 
-    } else {
-        echo "Por favor, preencha todos os campos do formulário de adicionar evento.";
-    }
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["adicionar_nota"])) {
-    if (!empty($_POST["conteudoNota"])) {
-        $contNota = $conn->real_escape_string($_POST["conteudoNota"]);
-
-
-        $sql = "INSERT INTO notes (note) VALUES ('$contNota')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Nova nota adicionado com sucesso!";
-        } else {
-            echo "Erro ao adicionar a nota: " . $conn->error;
-        }
-    } else {
-        echo "Por favor, preencha todos os campos do formulário de adicionar nota.";
-    }
 }
 ?>
