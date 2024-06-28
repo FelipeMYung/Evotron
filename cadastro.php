2446a768e64e6b410656151a06b1821e36bb1ded
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-06-28 08:28:35 -0300

    Login/cadastro
    .

--- cadastro.php
+++ cadastro.php
@@ -4,121 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="cadastro.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <title>Cadastro</title>
-    <style>p{
-    font-size: 30px;
-    font-family: Noto Sans, sans-serif;
-    text-align: justify;
-}
-.navbar-expand-lg{
-    background-color: #161616;
-}
-.nav-link{
-    font-size: 35px;
-}
-.navbar-brand{
-    font-size: 50px;
-}
-#continua-header{
-    background-color: #161616;
-}
-#header-h1{
-    color: white;
-    font-family: Noto Sans, sans-serif;
-    font-size: 150px;
-}
-#header-h2{
-    color: white;
-    font-family: Noto Sans, sans-serif;
-    font-size: 70px;
-}
-.span-verde{
-    color: #34AD29;
-}
-.span-roxo{
-    color: #481589;
-}
-#transicao-main{
-    background-image: linear-gradient(#929292, #c0c0c0, rgb(255, 255, 255));
-}
- 
-/* CADASTRO CSS */
- 
-.cadastro-corpo{
-    background-image:linear-gradient(#050505, #0c0c0c, #050505);
-}
-section{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    flex-flow: column;
-}
-.form-group {
-    position: relative;
-    margin: 12px, 0;
-}
-.form-group input {
-    padding: 8px 10px;
-    font-size: 18px;
-    border-radius: 5px;
-    border: #acacac solid 2px;
-    background-color: transparent;
-    color: #eee;
-    transition: 0.15s all ease;
-}
-.form-group input:focus {
-    border-color: #eee;
-}
-.form-group input::placeholder {
-    color: transparent;
-}
-.form-group .form-label {
-    position: absolute;
-    top: 9px;
-    left: 0;
-    font-size: 18px;
-    padding: 0 10px;
-    color: #acacac;
-    pointer-events: none;
-    transition: 0,15s all ease;
-}
-.form-group input:focus + .form-label,
-.form-group input:not(:placeholder-shown) +
-.form-label {
-    transform: translate(5px, -22px);
-    background-color: #0c0c0c;
-    font-size: 14px;
-    color: #acacac;
-}
-h4 {
-    font-size: 16px;
-    font-family: Noto Sans, sans-serif;
-}
-#h3-cadastro {
-    font-size: 25px;
-    font-family: Noto Sans, sans-serif;
-    color: #481589;
-}
-#link-cadastro-inicio {
-    text-decoration: none;
-    color: #acacac;
-}
-#link-cadastro-inicio:active {
-    text-decoration: none;
-}
-#link-cadastro-inicio:hover {
-    text-decoration: none;
-}
-</style>
 </head>
 <body class="cadastro-corpo">
     <main>
-    <section>
+    <section class="cadastro">
         <h3 id="h3-cadastro">Insira seu:</h3>
         <div class="form-group">
             <input type="text" placeholder="Full Name">
@@ -126,10 +19,34 @@ h4 {
         </div>
         <div class="form-group">
             <input type="email" placeholder="E-mail">
+            <label class="form-label">Senha</label>
+        </div>
+
+        <div class="form-group">
+            <input type="email" placeholder="E-mail">
             <label class="form-label">E-mail</label>
         </div>
-        <a href="Menu.php"><button class="btn btn-success btn-lg" style="width: 24.2vh" type="button">Cadastre-se</button></a>
-        <h4 class="mt-3">Já possui uma conta?<a href="login.html"> Faça login</a></h4>
+
+        <a href="Menu.php">
+            <button class="btn btn-success btn-lg" style="width: 40.2vh" type="button">Cadastre-se</button></a>
+        <a id="link-cadastro-inicio" href="Menu.php">Voltar para o início</a>
+    </section>
+
+    <section class="login">
+        <h3 id="h3-cadastro">Insira seu:</h3>
+        <div class="form-group">
+            <input type="text" placeholder="Full Name">
+            <label class="form-label">Nome completo</label>
+        </div>
+        <div class="form-group">
+            <input type="email" placeholder="E-mail">
+            <label class="form-label">Senha</label>
+        </div>
+
+        
+        <a href="Menu.php">
+            <button class="btn btn-success btn-lg" style="width: 40.2vh" type="button">Login</button></a>
+            
         <a id="link-cadastro-inicio" href="Menu.php">Voltar para o início</a>
     </section>
     </main>
--- index.php
+++ index.php
@@ -1,137 +1,141 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <title>Cadastro</title>
-    <style>p{
-    font-size: 30px;
-    font-family: Noto Sans, sans-serif;
-    text-align: justify;
-}
-.navbar-expand-lg{
-    background-color: #161616;
-}
-.nav-link{
-    font-size: 35px;
-}
-.navbar-brand{
-    font-size: 50px;
-}
-#continua-header{
-    background-color: #161616;
-}
-#header-h1{
-    color: white;
-    font-family: Noto Sans, sans-serif;
-    font-size: 150px;
-}
-#header-h2{
-    color: white;
-    font-family: Noto Sans, sans-serif;
-    font-size: 70px;
-}
-.span-verde{
-    color: #34AD29;
-}
-.span-roxo{
-    color: #481589;
-}
-#transicao-main{
-    background-image: linear-gradient(#929292, #c0c0c0, rgb(255, 255, 255));
-}
- 
-/* CADASTRO CSS */
- 
-.cadastro-corpo{
-    background-image:linear-gradient(#050505, #0c0c0c, #050505);
-}
-section{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    flex-flow: column;
-}
-.form-group {
-    position: relative;
-    margin: 12px, 0;
-}
-.form-group input {
-    padding: 8px 10px;
-    font-size: 18px;
-    border-radius: 5px;
-    border: #acacac solid 2px;
-    background-color: transparent;
-    color: #eee;
-    transition: 0.15s all ease;
-}
-.form-group input:focus {
-    border-color: #eee;
-}
-.form-group input::placeholder {
-    color: transparent;
-}
-.form-group .form-label {
-    position: absolute;
-    top: 9px;
-    left: 0;
-    font-size: 18px;
-    padding: 0 10px;
-    color: #acacac;
-    pointer-events: none;
-    transition: 0,15s all ease;
-}
-.form-group input:focus + .form-label,
-.form-group input:not(:placeholder-shown) +
-.form-label {
-    transform: translate(5px, -22px);
-    background-color: #0c0c0c;
-    font-size: 14px;
-    color: #acacac;
-}
-h4 {
-    font-size: 16px;
-    font-family: Noto Sans, sans-serif;
-}
-#h3-cadastro {
-    font-size: 25px;
-    font-family: Noto Sans, sans-serif;
-    color: #481589;
-}
-#link-cadastro-inicio {
-    text-decoration: none;
-    color: #acacac;
-}
-#link-cadastro-inicio:active {
-    text-decoration: none;
-}
-#link-cadastro-inicio:hover {
-    text-decoration: none;
-}
-tem menu de contexto</style>
-</head>
-<body class="cadastro-corpo">
-    <main>
-    <section>
-        <h3 id="h3-cadastro">Insira seu:</h3>
-        <div class="form-group">
-            <input type="text" placeholder="Full Name">
-            <label class="form-label">Nome completo</label>
-        </div>
-        <div class="form-group">
-            <input type="email" placeholder="E-mail">
-            <label class="form-label">E-mail</label>
-        </div>
-        <a href="Menu.php"><button class="btn btn-success btn-lg" style="width: 24.2vh" type="button">Cadastre-se</button></a>
-        <h4 class="mt-3">Já possui uma conta?<a href="login.html"> Faça login</a></h4>
-        <a id="link-cadastro-inicio" href="Menu.php">Voltar para o início</a>
-    </section>
-    </main>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                <span>Olá, Usuário</span> <!-- A mensagem "Olá, Usuário" deve ser dinâmica -->
+            </div>
+            <nav>
+                <ul>
+                <li ><a style="color:darkgrey;" href="Menu.php">HOME</a></li>
+                    <li><a href="cadastro.php">LOGIN</a></li>
+                    <li><a href="Sobre.php">SOBRE</a></li>
+                    <li><a href="#">CONTATO</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div class="popup-background" id="popup-background">
+                <div class="popup-main">
+                    <span id="popup-close">x</span>
+                    <h1 style="color: black;">Adicionar Tarefa:</h1>
+                    <form method="post" id="popup_inputs">
+                        <label for="titulo">Título:</label>
+                        <input type="text" id="titulo" name="titulo" required>
+                        <label for="descricao">Descrição:</label>
+                        <input type="text" id="descricao" name="descricao">
+                        <label for="data_vencimento">Data de Vencimento:</label>
+                        <input type="date" id="data_vencimento" name="data_vencimento">
+                        <label for="tipo_dado">Tipo de tarefa:</label>
+                        <select name="tipo_dado" id="tipo_dado">
+                            <option value="routine">Rotina</option>
+                            <option value="new">Novos</option>
+                        </select>
+                        <input type="submit" name="adicionar_tarefa" value="Adicionar">
+                    </form>
+                </div>
+            </div>
+            <div class="popup-background" id="agenda-popup-background">
+                <div class="popup-main">
+                    <span id="agenda-popup-close">x</span>
+                    <h1 style="color: black;"> Adicionar Evento na Agenda: </h1>
+                    <form method="post" action="" id="AgendaInputs">
+                        <label for="evento">Novo Evento:</label>
+                        <input type="text" id="evento" name="novoEvento" >
+                        <label for="dia">Dia:</label>
+                        <input type="date" id="dia" name="diaEvento" >
+                        <label for="hora">Hora:</label>
+                        <input type="time" id="hora" name="horaEvento" >
+                        <input type="submit" value="Adicionar" name="adicionar_evento">
+                    </form>
+                </div>
+            </div>
+            <div class="organization-container container">
+            <h2>ORGANIZAÇÃO</h2>
+            <div class="tasks-container">
+                <h3>TAREFAS:</h3>
+                <form method="GET" action="">
+                    <button type="submit" name="filter" value="all">Todos</button>
+                    <button type="submit" name="filter" value="new">Novos</button>
+                    <button type="submit" name="filter" value="routine">Rotina</button>
+                </form>
+            </div>
+            <div class="tasks-container2">
+                <button id="add-task-button">Adicionar Tarefa</button>
+            </div>
+            <ul id='lista_tarefas'>
+                <?php
+                include 'functions.php';
+
+                $filtro = isset($_GET['filter']) ? $_GET['filter'] : 'all';
+                $tarefas = buscarTarefas($filtro);
+
+                foreach ($tarefas as $tarefa) {
+                    echo "<li>
+                        <div class='task_block'>
+                            {$tarefa['title']} <br> {$tarefa['description']} <br> Data de Vencimento: {$tarefa['due_date']}
+                            <form method='post' action=''>
+                                <input type='hidden' name='titulo' value='{$tarefa['title']}'>
+                                <button type='submit' name='deletar_tarefa'>Delete</button>
+                            </form>
+                        </div>
+                    </li>";
+                }
+                ?>
+            </ul>
+
+                <div class='notes inputs'>
+                <form method="post" class="notes-container" class= "Bunton">
+                    <input type="text" name="tituloNota" placeholder="Título">
+                    <input type="text" id="main_notes" cols='2  ' name="conteudoNota" placeholder="Conteúdo">
+                    <button id="note_button" name="adicionar_nota"><p>Enviar</p></button>
+                </form>
+                <ul>
+                <?php
+                    $notas = buscarNotas();
+                    foreach($notas as $nota){
+                        echo "<li>{$nota['title']} - {$nota['note']}
+                        <form method='post' action=''>
+                            <input type='hidden' name='tituloNota' value='{$nota['title']}'>
+                            <button type='submit' name='deletar_nota'>Delete</button>
+                        </form>
+                        </li>";
+                    }
+                    ?>
+                </ul>
+                </div>
+            </div>
+            <div class="sidebar-container">
+                <div class="container schedule-container">
+                    <h2>AGENDA</h2>
+                    <button id="add-event-button">Adicionar Evento</button>
+                    <ul>
+                    <?php
+                    $eventos = buscarEvento();
+                    foreach($eventos as $evento){
+                        echo "<li>{$evento['title']} - {$evento['date_day']} - {$evento['date_hour']}
+                        <form method='post' action=''>
+                            <input type='hidden' name='novoEvento' value='{$evento['title']}'>
+                            <button type='submit' name='deletar_evento'>Delete</button>
+                        </form>
+                        </li> ";
+                    }
+                    ?>
+                </ul>
+                </div>
+                <div id="calendar"></div>
+                <h3 id="data"></h3>
+            </div>
+        </main>
+        <script src="script.js"></script>
+    </body>
+    </html>
